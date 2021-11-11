6ed8e47128c321d9a9c1f967c3f2ebefd0a6cc3d
Aakash Jha <aakash.jha1103@gmail.com>
2021-11-11 13:17:34 +0530

    Using clean-webpack-plugin
    On every `npm run build-prod`, new files will replace older ones.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './styles.css'
 console.log("This is a webpack demo")
 console.log("CSS is injected into the DOM as JS")
 console.log("Using webpack-dev-server in development mode")
+console.log('Also cleaning build folder on every PROD build')
